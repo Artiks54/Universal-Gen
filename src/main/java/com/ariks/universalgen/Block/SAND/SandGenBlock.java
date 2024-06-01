02d47fc4938e0c2e91cb7e73834f88bac33acc61
Artiks54 <novickoff.artiom2016@yandex.ru>
2024-06-01 16:39:19 +0300

    Update-1.1

---
+++ src/main/java/com/ariks/universalgen/Block/SAND/SandGenBlock.java
@@ -0,0 +1,99 @@
+package com.ariks.universalgen.Block.SAND;
+
+import com.ariks.universalgen.Block.COBBLE.CobbleGenTile;
+import com.ariks.universalgen.UniversalGen;
+import com.ariks.universalgen.Util.Config;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import java.util.List;
+
+public class SandGenBlock extends Block {
+    private final SandGenBlockEnum tileType;
+    public SandGenBlock(String name, SandGenBlockEnum tileType) {
+        super(Material.IRON);
+        this.setRegistryName(name);
+        this.tileType = tileType;
+        this.setUnlocalizedName(name);
+        this.setCreativeTab(UniversalGen.UniversalGenTab);
+        this.setHardness(1.5F);
+        this.setResistance(2.5f);
+        this.setSoundType(SoundType.METAL);
+        this.setHarvestLevel("shovel", 1);
+    }
+    @Override
+    public boolean onBlockActivated(@NotNull World worldIn, @NotNull BlockPos pos, @NotNull IBlockState state, @NotNull EntityPlayer playerIn, @NotNull EnumHand hand, @NotNull EnumFacing facing, float hitX, float hitY, float hitZ) {
+        TileEntity tile = worldIn.getTileEntity(pos);
+        if (!worldIn.isRemote && tile instanceof CobbleGenTile) {
+            int id = Integer.parseInt(((CobbleGenTile) tile).getGuiID());
+            playerIn.openGui(UniversalGen.instance, id, worldIn, pos.getX(), pos.getY(), pos.getZ());
+        }
+        return true;
+    }
+    @Override
+    public boolean hasTileEntity(@NotNull IBlockState state) {
+        return true;
+    }
+    @Override
+    public boolean canHarvestBlock(@NotNull IBlockAccess world, @NotNull BlockPos pos, @NotNull EntityPlayer player) {
+        return true;
+    }
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(@NotNull World world, @NotNull IBlockState state) {
+        CobbleGenTile tileEntity = new CobbleGenTile();
+        tileEntity.count = tileType.getCount();
+        return tileEntity;
+    }
+    @Override
+    public void breakBlock(World worldIn, @NotNull BlockPos pos, @NotNull IBlockState state) {
+        TileEntity tileEntity = worldIn.getTileEntity(pos);
+        assert tileEntity != null;
+        InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory) tileEntity);
+        super.breakBlock(worldIn, pos, state);
+    }
+    @Override
+    public void onBlockPlacedBy(@NotNull World worldIn, @NotNull BlockPos pos, @NotNull IBlockState state, @NotNull EntityLivingBase placer, @NotNull ItemStack stack) {
+        super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
+        if (Config.DebugMod && !worldIn.isRemote) {
+            UniversalGen.logger.info(
+                    "UniversalGen-log: Block place: " + getLocalizedName() + " Cord: " + pos.getX() + "," + pos.getY() + "," + pos.getZ() + " DismID: " + worldIn.provider.getDimension() + " PlayerName: " + placer.getName());
+        }
+    }
+    @Deprecated
+    public @NotNull AxisAlignedBB getBoundingBox(@NotNull IBlockState state, @NotNull IBlockAccess source, @NotNull BlockPos pos) {
+        return new AxisAlignedBB(0.062, 0, 0.062, 0.938, 0.875, 0.938);
+    }
+    @Deprecated
+    public boolean isOpaqueCube(@NotNull IBlockState state) {
+        return false;
+    }
+    @Override
+    public boolean isNormalCube(@NotNull IBlockState state, @NotNull IBlockAccess world, @NotNull BlockPos pos) {
+        return false;
+    }
+    @Deprecated
+    public boolean isFullCube(@NotNull IBlockState state) {
+        return false;
+    }
+    @Override
+    public void addInformation(@NotNull ItemStack stack, @Nullable World player, @NotNull List<String> tooltip, @NotNull ITooltipFlag advanced) {
+        tooltip.add("Generators: "+tileType.getCount()+" items");
+    }
+}
